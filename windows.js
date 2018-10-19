9f2dccba5383ebf8717aa80540f1751b4bb17605
hyd <hyd@hydde-MacBook-Pro.local>
2018-10-19 15:10:12 +0800

    homework1

---
+++ windows.js
@@ -0,0 +1,80 @@
+function show_topic() { 
+  var x = document.getElementById("classic1"); 
+  x.style.color = "#58FAF4"; 
+} 
+function myFunction(){document.getElementById("classic1");
+  alert("此句用比喻的手法,一筆勾勒出蜀道艱險難行的特徵,同時也明該詞標題");
+}
+
+function show_topic1() { 
+  var x = document.getElementById("classic2"); 
+  x.style.color = "#58FAF4"; 
+} 
+function myFunction1(){document.getElementById("classic2"); 
+  alert("第一句詩人將自己置身景中,對著西邊發現蜀道山路之狹窄陡峭;第二句用典介紹蜀道是由壯士鮮血澆築鋪墊來的,表現出詩人對於生命的同情");
+}
+
+function show_topic2() { 
+  var x = document.getElementById("classic3"); 
+  x.style.color = "#58FAF4"; 
+} 
+function myFunction2(){document.getElementById("classic3");
+  alert("作為本詞的經典句,該句再次用典,表現出劍閣的易守難攻和古代英雄豪傑保家衛國的壯志豪情");
+}
+
+function myFunction3(){$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideDown();});});}
+
+function myFunction4(){$(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panel1").slideDown();
+    });
+});}
+
+function showAnswer(){
+  var x=document.getElementById("word").value; 
+if(x=="詞牌名"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+function showAnswer1(){
+  var x=document.getElementById("word1").value; 
+if(x=="從那時以來"||x=="從那時起"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+function showAnswer2(){
+  var x=document.getElementById("word2").value; 
+if(x=="對著西邊"||x=="向西對著"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+function showAnswer3(){
+  var x=document.getElementById("word3").value; 
+if(x=="可怕的路途"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+function showAnswer4(){
+  var x=document.getElementById("word4").value; 
+if(x=="只聽見"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+function showAnswer5(){
+  var x=document.getElementById("word5").value; 
+if(x=="守關人"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+
+function showAnswer6(){
+  var x=document.getElementById("word6").value; 
+if(x=="下有衝波逆折之回川"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+function showAnswer7(){
+  var x=document.getElementById("word7").value; 
+if(x=="枯鬆倒掛倚絕壁"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
+function showAnswer8(){
+  var x=document.getElementById("word8").value; 
+if(x=="磨牙吮血,殺人如麻"){alert("正確☑️")}
+else{alert("錯誤❌")}
+}
